b6e9474ef57123c49cafb88ef464725fe2b66d49
aditya samal <aditya.samal@inovaare.com>
2026-07-01 00:55:38 +0530

    implememted fan-in/fan-out in go with 1 producer goroutine and 5 background goroutine

--- testingGo/channel1/channel1.go
+++ testingGo/channel1/channel1.go
@@ -5,48 +5,59 @@ import (
 	"sync"
 )
 
-func worker(id int,jobs <-chan int, results chan<-int, wg *sync.WaitGroup){
+func isPrime(x int) bool {
+	if x < 2 {
+		return false
+	}
+	if x == 2 {
+		return true
+	}
+	if x%2 == 0 {
+		return false
+	}
+	for j := 3; j*j <= x; j += 2 {
+		if x%j == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func worker(id int, jobs <-chan int, results chan<- int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	for job := range jobs{
-             fmt.Printf("worker %d started working..on job %d\n",id, job)
-			 x := job;
-			 count :=0;
-			 for j :=1;j<=x;j++{
-				if x%j==0{
-					count++
-				}
-			 }
-			 if count==2{
-                results <- x;
-			 }
+	for job := range jobs {
+		fmt.Printf("worker %d started working..on job %d\n", id, job)
+		if isPrime(job) {
+			results <- job
+		}
 	}
 
 }
 
-func main(){
+func main() {
 	fmt.Println("fan-in/fan-out 1 producer sending to 5 goroutines..")
-	jobs := make(chan int,5)
-	results := make(chan int,5)
+	jobs := make(chan int, 1000)
+	results := make(chan int, 1000)
 	//1 producer adds 2-1000000 to channel
-	go func(){
-		for i :=2;i<=100000;i++{
-		    jobs <- i
-	    }
+	go func() {
+		for i := 2; i <= 100000; i++ {
+			jobs <- i
+		}
 		close(jobs)
 	}()
-    var wg sync.WaitGroup;
+	var wg sync.WaitGroup
 
-	for w:=1;w<=5;w++{
+	for w := 1; w <= 1000; w++ {
 		wg.Add(1)
-		go worker(w,jobs,results,&wg)
+		go worker(w, jobs, results, &wg)
 	}
 	go func() {
 		wg.Wait()
 		close(results)
 	}()
-    cnt :=0;
-	for range results{
-        cnt++;
+	cnt := 0
+	for range results {
+		cnt++
 	}
 	fmt.Println(cnt)
 
